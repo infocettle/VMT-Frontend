eed5f5804725f4bf8979c5f7ae4894d2e4e9540e
Olaitan Dada <Michaeldada09@gmail.com>
2024-06-19 23:29:37 +0100

    new changes

--- src/pages/Dashboard/DashboardDiv.jsx
+++ src/pages/Dashboard/DashboardDiv.jsx
@@ -6,7 +6,7 @@ const DashboardDiv = ({ module }) => {
       {/* The div that renders Dashbaord flex */}
       <div className="w-full h-auto border border-gray-200 flex flex-col items-center">
         {/* Header Kinda */}
-        <div className="w-full py-3 px-12 flex items-center justify-between">
+        <div className="w-full py-3 pl-9 pr-12 flex items-center justify-between">
           <h2 className="dashboard-div-text uppercase">{module}</h2>
           <div className="flex w-auto space-x-2 items-center">
             <h2 className="dashboard-div-text-two">Dashboard</h2>

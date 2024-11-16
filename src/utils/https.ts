57fff3eb137599c11d9241a75627f33761b4a96e
candie <adekanmi.adewumi@straitpay.com>
2024-11-16 08:19:43 +0100

    url update

--- src/utils/https.ts
+++ src/utils/https.ts
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export const baseUrl = `http://localhost:3000/api/v1`;
+export const baseUrl = "https://vmt-server.onrender.com/api/v1/";
+// export const baseUrl = `http://localhost:3000/api/v1`;
